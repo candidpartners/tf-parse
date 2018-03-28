a9f4034f35c45189e1d756612369913ef0b3ed59
Alexander Higgins <TheAlexanderHiggins@gmail.com>
2018-03-28 16:58:20 -0400

    Updated pipeline

--- pipeline/publish-to-npm.sh
+++ pipeline/publish-to-npm.sh
@@ -2,7 +2,7 @@
 set -e
 
 npm version patch
-git commit -a -m "Bumped version"
+#git commit -a -m "Bumped version"
 version=$(cat ./package.json | jq '.version')
 git tag -a ${version} -m ${version}
 git push
