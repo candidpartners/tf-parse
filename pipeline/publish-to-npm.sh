55243620aa6233efeadceeeec9cd46d952004b73
Alexander Higgins <TheAlexanderHiggins@gmail.com>
2018-03-28 16:38:04 -0400

    Fixed publish script

--- pipeline/publish-to-npm.sh
+++ pipeline/publish-to-npm.sh
@@ -2,9 +2,9 @@
 set -e
 
 npm version patch
-#git commit -a -m "Bumped version"
-#version=$(cat ./package.json | jq '.version')
-#git tag -a ${version} -m ${version}
-#git push
-#npm publish
+git commit -a -m "Bumped version"
+version=$(cat ./package.json | jq '.version')
+git tag -a ${version} -m ${version}
+git push
+npm publish
 
